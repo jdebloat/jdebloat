d87f99c9d902f8cc572d70d7e545fbbe314d6121
Bobby R. Bruce <b.bruce@cs.ucla.edu>
2019-05-21 16:12:16 -0700

    inline database renamed to db.

--- data/inliner/settings.py
+++ data/inliner/settings.py
@@ -3,6 +3,6 @@ from inliner.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR)))), 'output', 'inliner.sqlite3'),
+        'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR)))), 'output', 'db.sqlite3'),
     }
 }
