f95d50497cb6528cf62a39a5bc370669f7d6e8df
Bobby R. Bruce <b.bruce@cs.ucla.edu>
2019-05-21 15:23:11 -0700

    Fixed inliner bugs.

--- data/inliner/settings.py
+++ data/inliner/settings.py
@@ -3,6 +3,6 @@ from inliner.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR)))), 'output', 'inliner', 'db.sqlite3'),
+        'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR)))), 'output', 'inliner.sqlite3'),
     }
 }
