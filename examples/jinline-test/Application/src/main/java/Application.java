06ae32e9b681660dbc023ea9691ea1d30d74cbd9
usama54321 <usama54321@gmail.com>
2020-07-16 20:22:58 +0000

    updated jinline-test example

--- examples/jinline-test/Application/src/main/java/Application.java
+++ examples/jinline-test/Application/src/main/java/Application.java
@@ -2,11 +2,11 @@ import edu.ucla.cs.onr.test.Library;
 
 public class Application {
 	public static void main(String[] args) {
-    System.out.println(square(2));
+    System.out.println(new Application().square(2));
 		return;
 	}
 
-  public static int square(int num) {
+  public int square(int num) {
     Library lib = new Library();
     return lib.square(num);
   }
--- examples/jinline-test/Application/src/test/java/ApplicationTest.java
+++ examples/jinline-test/Application/src/test/java/ApplicationTest.java
@@ -7,7 +7,13 @@ import java.io.IOException;
 
 public class ApplicationTest {
   @Test public void squareTest() {
+    for(int i = 0; i < 1_000_000; i++) {
+      assertEquals(square(2), 4);
+    }
+  }
+
+  private int square(int a) {
     Application app = new Application();
-    assertEquals(app.square(2), 4);
+    return app.square(a);
   }
 };
