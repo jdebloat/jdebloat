097608d106da23047940ea16bf654a5784f671a4
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-07-17 08:23:51 +0200

    Add JReduce example

---
+++ examples/jreduce-test/src/main/java/B.java
@@ -0,0 +1,6 @@
+public class B {
+  public String toString() {
+    new C();
+    return "Hello, B!";
+  }
+}
---
+++ examples/jreduce-test/src/main/java/C.java
@@ -0,0 +1,6 @@
+public class C {
+  public String toString() {
+    new A();
+    return "Hello, C!";
+  }
+}
